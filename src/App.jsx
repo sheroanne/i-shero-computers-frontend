3c2627ea4ddb29730e1316cdd99b662248fe9cc8
sheroanne <sheroanne18@gmail.com>
2025-10-12 11:34:03 +0530

    Shero

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import ProductCard from './components/productCard'
+
+function App() {
+
+  return (
+    <>
+   
+      
+     <ProductCard name="Laptop" price="100,000.00" image="https://picsum.photos/id/1/200/300"/>
+     <ProductCard name="Phone" price="90,000.00" image="https://picsum.photos/id/3/200/300"/>
+     <ProductCard name="Watch" price="5,000.00" image="https://picsum.photos/id/5/200/300"/>
+     
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/productCard.jsx
@@ -0,0 +1,11 @@
+export default function ProductCard(props){
+
+console.log(props);
+    return(
+        <div>
+        <h1>Laptop</h1>
+        <img src='https://picsum.photos/id/1/200/300' />
+        <p>Price LKR: 100 000.00</p>
+      </div>
+    );
+}
